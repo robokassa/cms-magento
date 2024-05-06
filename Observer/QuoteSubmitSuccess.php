39fecd531ffa4e29c55fbb7c166c380fcff2f191
Sonya <ssplavskaya@gmail.com>
2024-05-06 08:17:40 +0300

    disable observer QuoteSubmitSuccess

--- Observer/QuoteSubmitSuccess.php
+++ Observer/QuoteSubmitSuccess.php
@@ -10,16 +10,16 @@ class QuoteSubmitSuccess implements \Magento\Framework\Event\ObserverInterface
 
     public function execute(Observer $observer)
     {
-        /* @var Quote $quote */
-        $quote = $observer->getEvent()->getQuote();
-        /* @var \Magento\Sales\Model\Order $order */
-        $order = $observer->getEvent()->getOrder();
-        if ($quote &&
-            $order &&
-            $order->getPayment() &&
-            $order->getPayment()->getMethod() == \Astrio\Robokassa\Model\Payment\Method\Robokassa::PAYMENT_METHOD_CODE
-        ) {
-            $quote->setIsActive(true);
-        }
+        // /* @var Quote $quote */
+        // $quote = $observer->getEvent()->getQuote();
+        // /* @var \Magento\Sales\Model\Order $order */
+        // $order = $observer->getEvent()->getOrder();
+        // if ($quote &&
+        //     $order &&
+        //     $order->getPayment() &&
+        //     $order->getPayment()->getMethod() == \Astrio\Robokassa\Model\Payment\Method\Robokassa::PAYMENT_METHOD_CODE
+        // ) {
+        //     $quote->setIsActive(true);
+        // }
     }
 }
