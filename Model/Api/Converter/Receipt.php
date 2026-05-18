5051ba04630458c9d1cc697ad55ce0dea91ec9ac
Aleksey Dmitriev <73853919+dmtew@users.noreply.github.com>
2026-05-18 22:39:21 +0700

    fix(DEVIS-945): исправлено формирование номенклатуры фискального чека

--- Model/Api/Converter/Receipt.php
+++ Model/Api/Converter/Receipt.php
@@ -62,7 +62,7 @@ class Receipt
             $receiptItems[] = $this->getReceiptItem(
                 $orderItem->getName(),
                 // $orderItem->getBaseRowTotalInclTax(),
-                ['sum' => $orderItem->getRowTotalInclTax(), 'cost' => $orderItem->getPriceInclTax()],
+                $orderItem->getRowTotalInclTax(),
                 (int) $qty,
                 $tax,
                 $paymentMethod,
@@ -76,7 +76,7 @@ class Receipt
         if ($shippingPrice && $shippingPrice > 0) {
             $receiptItems[] = $this->getReceiptItem(
                 $order->getShippingDescription(),
-                ['sum' => $shippingPrice, 'cost' => $shippingPrice],
+                $shippingPrice,
                 1,
                 $tax,
                 $paymentMethod,
@@ -93,33 +93,21 @@ class Receipt
      * Get receipt item.
      *
      * @param string $name
-     * @param float $sum
+     * @param float $amount
      * @param int $quantity
      * @param string $tax
      * @param string|null $paymentMethod
      * @param string|null $paymentObject
      * @return array
      */
-    protected function getReceiptItem($name, $amounts, $quantity, $tax, $paymentMethod, $paymentObject)
+    protected function getReceiptItem($name, $amount, $quantity, $tax, $paymentMethod, $paymentObject)
     {
-        if (is_array($amounts)) {
-            $sum  = $amounts['sum'];
-            $cost = $amounts['cost'];
-        } else {
-            $sum  = $amounts;
-            $cost = null;
-        }
-
         $receiptItem = [
             'name'     => mb_substr(trim(htmlspecialchars($name)), 0, 127, 'UTF-8'),
-            'sum'      => round($sum, 2),
+            'amount'   => round($amount, 2),
             'quantity' => $quantity,
             'tax'      => $tax
         ];
-
-        if (!is_null($cost)) {
-            $receiptItem['cost'] = round($cost, 2);
-        }
 
         if ($paymentMethod) {
             $receiptItem['payment_method'] = $paymentMethod;
@@ -135,29 +123,75 @@ class Receipt
     protected function correctingReceiptAmounts($order, $receiptItems)
     {
         $orderAmount = $order->getGrandTotal();
-        $itemsAmount = array_sum(array_column($receiptItems, 'sum'));
+        $itemsAmount = array_sum(array_column($receiptItems, 'amount'));
 
         $discount = $orderAmount - $itemsAmount;
-        if ($discount == 0) {
-            return $receiptItems;
+        if ($discount != 0) {
+            $remaindertDiscount = $discount;
+            foreach ($receiptItems as $index => &$receiptItem) {
+                if ($index < count($receiptItems) - 1) {
+                    $percentInOrder = $receiptItem['amount'] / $itemsAmount * 100;
+                    $correctAmount  = round($percentInOrder * $discount / 100, 2);
+                } else {
+                    $correctAmount = $remaindertDiscount;
+                }
+
+                $receiptItem['amount'] += $correctAmount;
+
+                $remaindertDiscount -= $correctAmount;
+            }
+            unset($receiptItem);
         }
 
-        $remaindertDiscount = $discount;
-        foreach ($receiptItems as $index => &$receiptItem) {
-            if ($index < count($receiptItems) - 1) {
-                $percentInOrder = $receiptItem['sum'] / $itemsAmount * 100;
-                $correctAmount  = round($percentInOrder * $discount / 100, 2);
-            } else {
-                $correctAmount = $remaindertDiscount;
+        return $this->convertReceiptItemsToCosts($receiptItems, $orderAmount);
+    }
+
+    protected function convertReceiptItemsToCosts($receiptItems, $orderAmount)
+    {
+        $calculatedAmount = 0;
+        foreach ($receiptItems as &$receiptItem) {
+            $quantity = max(1, (int) $receiptItem['quantity']);
+            $amount = (int) round($receiptItem['amount'] * 100);
+            $cost = (int) round($amount / $quantity);
+
+            $receiptItem['cost'] = round($cost / 100, 2);
+            $calculatedAmount += $cost * $quantity;
+            unset($receiptItem['amount']);
+        }
+        unset($receiptItem);
+
+        $diff = (int) round($orderAmount * 100) - $calculatedAmount;
+        foreach ($receiptItems as &$receiptItem) {
+            $quantity = max(1, (int) $receiptItem['quantity']);
+            if ($diff != 0 && $diff % $quantity == 0) {
+                $receiptItem['cost'] = round($receiptItem['cost'] + ($diff / $quantity / 100), 2);
+                $diff = 0;
+                break;
+            }
+        }
+        unset($receiptItem);
+
+        if ($diff != 0) {
+            $receiptItems = $this->splitReceiptItemForRoundingDiff($receiptItems, $diff);
+        }
+
+        return $receiptItems;
+    }
+
+    protected function splitReceiptItemForRoundingDiff($receiptItems, $diff)
+    {
+        foreach ($receiptItems as $index => $receiptItem) {
+            $quantity = max(1, (int) $receiptItem['quantity']);
+            if ($quantity <= 1) {
+                continue;
             }
 
-            $receiptItem['sum'] += $correctAmount;
-
-            if (isset($receiptItem['cost'])) {
-                $receiptItem['cost'] = round($receiptItem['sum'] / $receiptItem['quantity'], 2);
-            }
-
-            $remaindertDiscount -= $correctAmount;
+            $splitItem = $receiptItem;
+            $receiptItems[$index]['quantity'] = $quantity - 1;
+            $splitItem['quantity'] = 1;
+            $splitItem['cost'] = round($splitItem['cost'] + ($diff / 100), 2);
+            array_splice($receiptItems, $index + 1, 0, [$splitItem]);
+            break;
         }
 
         return $receiptItems;
